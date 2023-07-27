d6b7fee8be135c683212af55a0668cd7ed75e3db
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-07-27 09:09:16 +0200

    Change background color and add grid

--- src/pages/HomePage/ProjectsReview/index.jsx
+++ src/pages/HomePage/ProjectsReview/index.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 export const ProjectsReview = () => {
   return (
     <section className="projects-review">
-      <button className="projects-review__project project1"></button>
-      <button className="projects-review__project project2"></button>
-      <button className="projects-review__project project3"></button>
-      <button className="projects-review__project project4"></button>
-      <button className="projects-review__project project5"></button>
-      <button className="projects-review__project project6"></button>
-      <button className="projects-review__project project7"></button>
-      <button className="projects-review__project project8"></button>
-      <button className="projects-review__project projects-review__btn"></button>
+      <h2>Projects</h2>
+      <section className="projects-review__box">
+        <button className="projects-review__project project1">1</button>
+        <button className="projects-review__project project2">2</button>
+        <button className="projects-review__project project3">3</button>
+        <button className="projects-review__project project4">4</button>
+        <button className="projects-review__project project5">5</button>
+      </section>
+      <Link
+        to="/projects"
+        className="projects-review__project projects-review__btn"
+      >
+        Více projektů
+      </Link>
     </section>
   );
 };
