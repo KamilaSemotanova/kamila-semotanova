403f06360046f3f2a6ebadbc6824915c79f96002
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-13 23:28:56 +0200

    Extract button from box in home-about

--- src/pages/HomePage/HomeAbout/index.jsx
+++ src/pages/HomePage/HomeAbout/index.jsx
@@ -7,19 +7,17 @@ export const HomeAbout = () => {
     <section className="home-about">
       <div className="home-about__photo"></div>
       <div className="home-about__textbox">
-        <div>
-          <h2 className="home-about__greeting">Hello,</h2>
-          <p>
-            I am an enthusiastic Junior Front-End Developer with a blend of
-            skills that extend seamlessly into the realm of UX/UI. My meticulous
-            attention to detail, coupled with an insatiable eagerness to learn,
-            sets me apart in my pursuit of excellence.
-          </p>
-        </div>
-        <Link to={'/about'} className="home-about__btn">
-          More about me
-        </Link>
+        <h2 className="home-about__greeting">Hello,</h2>
+        <p>
+          I am an enthusiastic Junior Front-End Developer with a blend of skills
+          that extend seamlessly into the realm of UX/UI. My meticulous
+          attention to detail, coupled with an insatiable eagerness to learn,
+          sets me apart in my pursuit of excellence.
+        </p>
       </div>
+      <Link to={'/about'} className="home-about__btn">
+        More about me
+      </Link>
     </section>
   );
 };
