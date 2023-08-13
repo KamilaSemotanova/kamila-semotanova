044442a6474b7ce6f9ed4ec65a707d95095cb744
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-13 12:29:42 +0200

    Add contact list

--- src/pages/HomePage/Contact/index.jsx
+++ src/pages/HomePage/Contact/index.jsx
@@ -5,7 +5,14 @@ import { Form } from './Form';
 export const Contact = () => {
   return (
     <section className="contact">
+      <h1 className="contact__title">Contact</h1>
       <Form />
+      <div className='contact__box'>
+        <p>E-mail: kamila.semotanova@gmail.com</p>
+        <p>LinkedIn:</p>
+        <p>GitHub:</p>
+        <p>Figma:</p>
+      </div>
     </section>
   );
 };
