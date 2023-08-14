51a8a8b6d32c44b4579b6da6d114adf963d8a8be
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-14 13:26:44 +0200

    Make sense and style in aboutpage

--- src/pages/HomePage/Contact/index.jsx
+++ src/pages/HomePage/Contact/index.jsx
@@ -4,10 +4,10 @@ import { Form } from './Form';
 
 export const Contact = () => {
   return (
-    <section className="contact">
+    <section className="contact" id="contact">
       <h1 className="contact__title">Contact</h1>
       <Form />
-      <div className='contact__box'>
+      <div className="contact__box">
         <p>E-mail: kamila.semotanova@gmail.com</p>
         <p>LinkedIn:</p>
         <p>GitHub:</p>
