1f16746bb99b760ce064233906a9977b644eb316
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-14 11:22:24 +0200

    Add responsivity, style and function to the error page

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 export const ErrorPage = () => {
   return (
-    <section className="error__picture">
+    <section className="error">
       <div className="error__box">
-        <p>404 </p>
-        <p>Page not found</p>
+        <p className="error__title">404</p>
+        <div className="error__container">
+          <p className="error__text">page not found.</p>
+          <Link to={'/'} className="error__btn">
+            home
+          </Link>
+        </div>
       </div>
     </section>
   );
