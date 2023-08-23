92542ff72fd1f6d8e5feb301ebe8ebab98faddf9
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-23 20:26:13 +0200

    Make about in home responsive for mobile

--- src/pages/HomePage/HomeAbout/index.jsx
+++ src/pages/HomePage/HomeAbout/index.jsx
@@ -7,19 +7,21 @@ export const HomeAbout = () => {
     <section className="home-about">
       <div className="home-about__photo"></div>
       <div>
-        <div className="home-about__textbox">
-          <p className="home-about__quotation quotation-first">“</p>
-          <div>
-            <h2 className="home-about__greeting">Hello,</h2>
-            <p className="home-about__text">
-              my name is Kamila and I am an Junior Front-End Developer with
-              skills of UX/UI designer.
-            </p>
-            <Link to={'/about'} className="home-about__btn">
-              More about me
-            </Link>
+        <div className="home-about__introbox">
+          <div className="home-about__textbox">
+            <p className="home-about__quotation quotation-first">“</p>
+            <div className="home-about__introbox">
+              <h2 className="home-about__greeting">Hello,</h2>
+              <p className="home-about__text">
+                my name is Kamila and I am an Junior Front-End Developer with
+                skills of UX/UI designer.
+              </p>
+            </div>
+            <p className="home-about__quotation quotation-second">”</p>
           </div>
-          <p className="home-about__quotation quotation-second">”</p>
+          <Link to={'/about'} className="home-about__btn">
+            More about me
+          </Link>
         </div>
       </div>
     </section>
