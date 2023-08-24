bcfe17eaaaf2ccd5ca39e2b3013c7cf4fa6a8559
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-24 17:07:53 +0200

    Add new styling to home about -- all finished

--- src/pages/HomePage/HomeAbout/index.jsx
+++ src/pages/HomePage/HomeAbout/index.jsx
@@ -6,23 +6,21 @@ export const HomeAbout = () => {
   return (
     <section className="home-about">
       <div className="home-about__photo"></div>
-      <div>
-        <div className="home-about__container">
-          <div className="home-about__textbox">
-            <p className="home-about__quotation quotation-first">“</p>
-            <div className="home-about__introbox">
-              <h2 className="home-about__greeting">Hello,</h2>
-              <p className="home-about__text">
-                my name is Kamila and I am an Junior Front-End Developer with
-                skills of UX/UI designer.
-              </p>
-            </div>
-            <p className="home-about__quotation quotation-second">”</p>
+      <div className="home-about__container">
+        <div className="home-about__textbox">
+          <p className="home-about__quotation quotation-first">“</p>
+          <div className="home-about__introbox">
+            <h2 className="home-about__greeting">Hello,</h2>
+            <p className="home-about__text">
+              my name is Kamila and I am an Junior Front-End Developer with
+              skills of UX/UI designer.
+            </p>
           </div>
-          <Link to={'/about'} className="home-about__btn">
-            More about me
-          </Link>
+          <p className="home-about__quotation quotation-second">”</p>
         </div>
+        <Link to={'/about'} className="home-about__btn">
+          More about me
+        </Link>
       </div>
     </section>
   );
