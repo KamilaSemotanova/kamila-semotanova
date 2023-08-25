2dc226211e7c0913350bc7a25f71971321256748
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-25 08:15:45 +0200

    Add link to favicon

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -13,76 +13,52 @@ export const AboutPage = () => {
     <section className="about-page">
       <div className="about-page__box">
         <div className="about-page__container">
-          <div className="about-page__title-box first-box">
-            <div className="about-page__photo first"></div>
-          </div>
-          <h3 className="about-page__title">Who am I?</h3>
-          <div className="about-page__section">
-            <p>
-              My name is Kamila Semotánová and I was born and reised in Prague,
-              Czech Republic. Over all I am not really a fan of classic sports
-              with ball. Through out the time I found my way to sports: in
-              summer I love to ride a bycicle and in winter is my passion to
-              snowboard.
-            </p>
-          </div>
+          <div className="about-page__photo first"></div>
+          <p>
+            My name is Kamila Semotánová and I was born and reised in Prague,
+            Czech Republic. Over all I am not really a fan of classic sports
+            with ball. Through out the time I found my way to sports: in summer
+            I love to ride a bycicle and in winter is my passion to snowboard.
+          </p>
         </div>
         <div className="about-page__container">
-          <div className="about-page__title-box second-box">
-            <h3 className="about-page__title">My work expierence so far?</h3>
-          </div>
-          <div className="about-page__section">
-            <p>
-              My working history is mainly landed in gastro industry, where i
-              mastered my skills in comunication, work in team and problem
-              solving skills. Covid got me looking for different direction than
-              before and I found myself trying many different positions which
-              included managing covid lab and planting trees. I landed my self
-              position in small startup where i learned more about development
-              and also got in charge of creating HTML e-mails.
-            </p>
-          </div>
+          <p>
+            My working history is mainly landed in gastro industry, where i
+            mastered my skills in comunication, work in team and problem solving
+            skills. Covid got me looking for different direction than before and
+            I found myself trying many different positions which included
+            managing covid lab and planting trees. I landed my self position in
+            small startup where i learned more about development and also got in
+            charge of creating HTML e-mails.
+          </p>
         </div>
         <div className="about-page__container">
-          <div className="about-page__title-box third-box">
-            <h3 className="about-page__title">
-              Where did passion for IT came from?
-            </h3>
-          </div>
-          <div className="about-page__section">
-            <p>
-              My passion for IT lays in my obsesion to solve problem layed infront of me.
-            </p>
-          </div>
+          <p>
+            My passion for IT lays in my obsesion to solve problem layed infront
+            of me.
+          </p>
         </div>
         <div className="about-page__container">
-          <div className="about-page__title-box fourth-box">
-            <div className="about-page__photo fourth"></div>
-          </div>
-          <h3 className="about-page__title">What are my skills?</h3>
+          <div className="about-page__photo fourth"></div>
+          <p>
+            After finishing my courses I dive myself into offering my skills to
+            other people that need to create websites. I was very lucky to begin
+            colaboration with BoomEvents that offered me to convey projets of
+            their clients. With that in stock I also started to putting the word
+            out and getting more projects by myself.
+          </p>
+          <Link to={'../projects'} className="about-page__link">
+            see all projects
+          </Link>
+        </div>
+        <div className="about-page__container">
+          <div className="about-page__title-box fourth-box"></div>
           <div className="about-page__section">
+            <h2>Skills</h2>
             <ul>
               <li>1</li>
               <li>2</li>
             </ul>
-          </div>
-        </div>
-        <div className="about-page__container">
-          <div className="about-page__title-box fifth-box">
-            <h3 className="about-page__title">What did I already achieved?</h3>
-          </div>
-          <div className="about-page__section">
-            <p>
-              After finishing my courses I dive myself into offering my skills
-              to other people that need to create websites. I was very lucky to
-              begin colaboration with BoomEvents that offered me to convey
-              projets of their clients. With that in stock I also started to
-              putting the word out and getting more projects by myself.{' '}
-            </p>
-            <p>You can browse through all projects here:</p>
-            <Link to={'../projects'} className="about-page__link">
-              Projects
-            </Link>
           </div>
         </div>
         <div className="about-page__container">
