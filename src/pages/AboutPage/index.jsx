82c91d303da8651711deb650f29367fa797a6d27
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-08-25 17:20:13 +0200

    Start with finalising mobile style of about page

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -12,17 +12,15 @@ export const AboutPage = () => {
   return (
     <section className="about-page">
       <div className="about-page__box">
+        <div className="about-page__photo first"></div>
         <div className="about-page__container">
-          <div className="about-page__photo first"></div>
-          <p>
+          <p className="about-page__text">
             My name is Kamila Semotánová and I was born and reised in Prague,
             Czech Republic. Over all I am not really a fan of classic sports
             with ball. Through out the time I found my way to sports: in summer
             I love to ride a bycicle and in winter is my passion to snowboard.
           </p>
-        </div>
-        <div className="about-page__container">
-          <p>
+          <p className="about-page__text">
             My working history is mainly landed in gastro industry, where i
             mastered my skills in comunication, work in team and problem solving
             skills. Covid got me looking for different direction than before and
@@ -31,56 +29,44 @@ export const AboutPage = () => {
             small startup where i learned more about development and also got in
             charge of creating HTML e-mails.
           </p>
-        </div>
-        <div className="about-page__container">
-          <p>
+          <p className="about-page__text">
             My passion for IT lays in my obsesion to solve problem layed infront
             of me.
           </p>
         </div>
+        <div className="about-page__photo second"></div>
         <div className="about-page__container">
-          <div className="about-page__photo fourth"></div>
-          <p>
+          <p className="about-page__text">
             After finishing my courses I dive myself into offering my skills to
             other people that need to create websites. I was very lucky to begin
             colaboration with BoomEvents that offered me to convey projets of
             their clients. With that in stock I also started to putting the word
             out and getting more projects by myself.
           </p>
-          <Link to={'../projects'} className="about-page__link">
+          <Link to={'../projects'} className="about-page__btn">
             see all projects
           </Link>
+          <button className="about-page__btn" onClick={handleDownload}>
+            Download CV
+          </button>
         </div>
         <div className="about-page__container">
-          <div className="about-page__title-box fourth-box"></div>
-          <div className="about-page__section">
-            <h2>Skills</h2>
-            <ul>
-              <li>1</li>
-              <li>2</li>
-            </ul>
-          </div>
+          <h2>Skills</h2>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+          </ul>
         </div>
+        <div className="about-page__photo third"></div>
         <div className="about-page__container">
-          <div className="about-page__title-box sixth-box">
-            <div className="about-page__photo sixth"></div>
-          </div>
-          <h3 className="about-page__title">
-            Would you like to get in touch with?
-          </h3>
-          <div className="about-page__section">
-            <p>
-              Write me a message whether you would like me to create you a
-              website or just to get in touch.
-            </p>
-            <HashLink to={'../#contact'} className="about-page__link">
-              Contact
-            </HashLink>
-          </div>
+          <p className="about-page__text">
+            Write me a message whether you would like me to create you a website
+            or just to get in touch.
+          </p>
+          <HashLink to={'../#contact'} className="about-page__btn">
+            Contact me
+          </HashLink>
         </div>
-        <button className="about-page__btn" onClick={handleDownload}>
-          Download CV
-        </button>
       </div>
       <Footer />
     </section>
