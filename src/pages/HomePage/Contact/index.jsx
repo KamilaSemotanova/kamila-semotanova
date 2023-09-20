1ce27d8b2fe7b8f65cf349aaedf5b5aaaa91ee77
Kamila Semotánová <kamila.semotanova@gmail.com>
2023-09-20 18:10:15 +0200

    Finish content of contact section

--- src/pages/HomePage/Contact/index.jsx
+++ src/pages/HomePage/Contact/index.jsx
@@ -8,10 +8,41 @@ export const Contact = () => {
       <h1 className="contact__title">Contact</h1>
       <Form />
       <div className="contact__box">
-        <p>E-mail: kamila.semotanova@gmail.com</p>
-        <p>LinkedIn:</p>
-        <p>GitHub:</p>
-        <p>Figma:</p>
+        <div>
+          {' '}
+          <img
+            src={require('./img/mail.png')}
+            alt="email"
+            className="contact__icon"
+          />
+          <p>kamila.semotanova@gmail.com</p>
+        </div>
+        <div>
+          <img
+            src={require('./img/social-linkedin.png')}
+            alt="linkedin"
+            className="contact__icon"
+          />
+          <a href="https://www.linkedin.com/in/kamila-semotánová/">LinkedIn</a>
+        </div>
+        <div>
+          <img
+            src={require('./img/social-github.png')}
+            alt="github"
+            className="contact__icon"
+          />
+          <a href="https://github.com/KamilaSemotanova">GitHub</a>
+        </div>
+        <div>
+          <img
+            src={require('./img/social-figma-1.png')}
+            alt="figma"
+            className="contact__icon"
+          />
+          <a href="https://www.figma.com/files/drafts?fuid=1222931808663192972">
+            Figma
+          </a>
+        </div>
       </div>
     </section>
   );
